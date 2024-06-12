782665de5e41ba31de1c3a9b18c332487340a364
Hildan Kusto Utomo <109232248+hildanku@users.noreply.github.com>
2024-06-12 06:23:22 +0000

    feat(main): added function to read data csv

--- main.go
+++ main.go
@@ -1,26 +1,69 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 )
 
+func readWhitelistCSV(filePath string) ([]WhiteList, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	// Skip header row if present
+	_, err = reader.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	var whitelist []WhiteList
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		whitelist = append(whitelist, WhiteList{Link: record[0]})
+	}
+
+	return whitelist, nil
+}
+
+type WhiteList struct {
+	Link string
+}
+
 func main() {
+
+	whitelist, err := readWhitelistCSV("data.csv")
+	if err != nil {
+		panic(err)
+	}
+
 	var link string
 	fmt.Println("Masukkan link yang ingin dianalisis:")
 	fmt.Scanln(&link)
-	fmt.Println("analyze:", link)
-	var data = [...]string{"dana.id", "test.aja"}
 
-	// var status bool
-	status := false
-	for _, check := range data {
-		if strings.Contains(link, check) {
-			fmt.Println("URL aman")
-			status = true
+	isSafe := false
+	for _, whitelistItem := range whitelist {
+		if strings.Contains(link, whitelistItem.Link) {
+			isSafe = true
+			break
 		}
 	}
-	if !status {
-		fmt.Println("Url Berbhaya")
+
+	if isSafe {
+		fmt.Println("URL aman")
+	} else {
+		fmt.Println("URL Berbahaya")
 	}
 }
